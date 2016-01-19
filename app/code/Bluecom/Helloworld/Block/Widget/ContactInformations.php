f530734dbbdb95284cb9eb2c42c8f494f4b6be8a
Hien Truong <hien.truong@bluecomgroup.com>
2016-01-19 10:29:22 +0700

    Create Widget

--- app/code/Bluecom/Helloworld/Block/Widget/ContactInformations.php
+++ app/code/Bluecom/Helloworld/Block/Widget/ContactInformations.php
@@ -3,6 +3,9 @@ namespace Bluecom\Helloworld\Block\Widget;
 
 class ContactInformations extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
+    /**
+     * Get block contact information
+     */
     public function _toHtml()
     {
         $this->setTemplate('widget/contact_informations.phtml');
--- app/code/Bluecom/Helloworld/Model/Config/Source/Gender.php
+++ app/code/Bluecom/Helloworld/Model/Config/Source/Gender.php
@@ -3,6 +3,11 @@ namespace Bluecom\Helloworld\Model\Config\Source;
 
 class Gender implements \Magento\Framework\Option\ArrayInterface
 {
+    /**
+     * Create option to contact form
+     *
+     * @return array
+     */
     public function toOptionArray()
     {
         return [['value' => 'mal', 'label' => __('Male')],
